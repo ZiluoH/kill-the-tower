6e4d3f2b68834bb790e7d5d4e2d7849b6d91418f
kgodby <kylegodby1@gmail.com>
2020-01-21 13:54:53 -0800

    made start page

---
+++ frontend/src/components/card/card.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+class Card extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
+
+    componentWillMount() {
+        //retrieve card from db and set state
+    }
+
+
+    render() {
+        return (
+            <div className="card-frame">
+                <div className="card-heading">
+                    <div id="card-cost">{this.state.cost}</div>
+                    <div id="card-name">{this.state.name}</div>
+                </div>
+                <div className="card-text">{this.state.</div>
+            </div>
+        )
+    }
+}
--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import './main.css'
 
 class MainPage extends React.Component {
 
     render() {
         return (
-            <div>
-                <h1>Kill the Tower</h1>
+            <div id="main-page">
+                
+                <header id="main-title-header">
+                    <h1 id="main-title">Terminate the Tower</h1>
+                </header>
+                <div id="min-form-container">
+                    <form id="min-form" action="">
+                        <div className="btn" id="start">Start</div>
+                        <div className="btn" id="login">Login</div>
+                    </form>
+                </div>
         
             </div>
         );
