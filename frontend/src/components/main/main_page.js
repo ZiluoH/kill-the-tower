dcc88a42a8e190e2bf8523423604795e1435ba6f
Richard <ziluoh@gmail.com>
2020-01-21 11:20:59 -0800

    Setup all files

---
+++ frontend/src/components/main/main_page.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Kill the Tower</h1>
+        
+            </div>
+        );
+    }
+}
+
+export default MainPage;
