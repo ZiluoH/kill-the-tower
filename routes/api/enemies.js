3122aa6ce323d1ba7d90722b6a32887555137a8e
xkylesun <xyasun@gmail.com>
2020-01-21 20:10:34 -0800

    same as above

--- routes/api/enemies.js
+++ routes/api/enemies.js
@@ -1,15 +1,15 @@
-// const express = require("express");
-// const router = express.Router();
-// const mongoose = require("mongoose");
-// const passport = require("passport");
-// const Enemy = require("../../models/Enemy");
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+const Enemy = require("../../models/Enemy");
 
-// router.get("/:type", (req, res) => {
-//   Enemy.find({ type: req.params.type })
-//     .then(enemies => res.json(enemies))
-//     .catch(err =>
-//       res.status(404).json({ noenemiesfound: "No enemy ???!!!??!!!" })
-//     );
-// });
+router.get("/:type", (req, res) => {
+  Enemy.find({ type: req.params.type })
+    .then(enemies => res.json(enemies))
+    .catch(err =>
+      res.status(404).json({ noenemiesfound: "No enemy ???!!!??!!!" })
+    );
+});
 
-// module.exports = router;
+module.exports = router;
