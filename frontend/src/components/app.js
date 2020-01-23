7ed5fdb3d7d0d0c0ba3edfc9b36a9b75d1301e82
xkylesun <xyasun@gmail.com>
2020-01-23 15:36:01 -0800

    map done

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -7,6 +7,7 @@ import MainPage from './main/main_page';
 import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
 import ProfileContainer from './profile/profile_container';
+import MapContainer from "./map/map_container";
 import TweetComposeContainer from './tweets/tweet_compose_container';
 
 
@@ -16,6 +17,7 @@ const App = () => (
       <Route exact path="/" component={MainPage} />
       <Route exact path="/login" component={LoginFormContainer} />
       <Route exact path="/signup" component={SignupFormContainer} />
+      <Route exect path="/map" component={MapContainer} />
       <ProtectedRoute exact path="/tweets" component={TweetsContainer} />
       <ProtectedRoute exact path="/profile" component={ProfileContainer} />
       <ProtectedRoute exact path="/new_tweet" component={TweetComposeContainer} />
